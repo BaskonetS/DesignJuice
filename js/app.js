2376de045d959fc4b40ac6513c7917f5e2b6fba6
BaskonetS <iga123mboy@gmail.com>
2021-03-02 02:19:50 +0300

    new project

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$ (function() {
+
+  let reviews__slider = $("#reviews__slider");
+
+  reviews__slider.slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 10000,
+      arrows: true,     
+      //fade:true,
+  }); 
+
+
+});
