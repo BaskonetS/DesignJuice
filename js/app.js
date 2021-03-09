39719eadd088e336aa198effd42307fbe5fb8c5f
BaskonetS <iga123mboy@gmail.com>
2021-03-09 04:01:17 +0300

    responsive

--- js/app.js
+++ js/app.js
@@ -9,7 +9,48 @@ $ (function() {
       autoplaySpeed: 10000,
       arrows: true,     
       //fade:true,
+
+      responsive: [
+        {
+          breakpoint: 1700,
+          settings: {  
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {  
+            slidesToShow: 1,
+            fade:true,            
+          }
+        },
+        
+      ]
   }); 
+
+  $('.header-burger').click(function(event) {
+    $('.header-burger, .header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+
+  $('.header__nav').click(function(){
+    $('.header-burger, .header__menu').removeClass('active');
+    $('body').removeClass('lock');
+  });  
+
+  /* smooth scroll */
+
+  $("[data-scroll]").on("click", function(event) {
+    event.preventDefault();
+    let elementId= $(this).data('scroll');
+    let elementOffset= $(elementId).offset().top;
+    
+
+    $("html, body").animate({
+      scrollTop: elementOffset- 70 
+    }, 700);
+
+  });
 
 
 });
